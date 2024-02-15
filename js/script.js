06ba63116c01bf4647006e4750f9feb0f1ace0c6
Richard Duarte <126694700+RichardDuarteDev@users.noreply.github.com>
2024-02-14 23:42:05 -0300

    Commit relese v-3.0
    Commit relese v-3.0

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+  }
+
+  function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+  }
+
+  function closeSidebar() {
+    document.getElementById('sidebar').style.width = '0';
+  }
